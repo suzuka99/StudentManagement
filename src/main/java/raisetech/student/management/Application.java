77541626d0a78da24c003e1ea1c741ba5c23f7cc
suzuka99 <kaiseinhk@icloud.com>
2025-10-01 15:54:39 +0900

    講座20の内容を反映

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -4,11 +4,13 @@
 // POSTは情報を与える、渡す
 package raisetech.student.management;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,27 +19,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	private String name = "Suzuki Kaisei";
-	private String age = "25";
+	@Autowired
+	private StudentRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-		return name + " " + age + "歳";
+	/* // 全件取得
+	@GetMapping("/studentAll")
+	public List<Student> getAllStudent() {
+		return repository.serchByAll();
+	} */
+
+	// 指定した名前の取得
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name) {
+		Student student = repository.serchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
-	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name, String age) {
-		this.name = name;
-		this.age = age;
+	// データの追加
+	@PostMapping("/student")
+	public void registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
 	}
 
-	// nameだけ更新
-	@PostMapping("/studentName")
-	public void updateStudentName(String name) {
-		this.name = name;
+	/* // 名前の変更
+	@PatchMapping("/studentName")
+	public void updateStudentName(String name, int age) {
+		repository.updateStudentName(name, age);
+	} */
+
+	// 年齢の変更
+	@PatchMapping("/studentAge")
+	public void updateStudentAge(String name, int age) {
+		repository.updateStudentAge(name, age);
+	}
+
+	// データの削除
+	@DeleteMapping("/student")
+	public void deleteStudent(String name) {
+		repository.deleteStudent(name);
 	}
 }
