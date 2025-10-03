8b7e171220ffd0fc54739a85d336fdc9c736ccc9
suzuka99 <kaiseinhk@icloud.com>
2025-10-03 13:06:33 +0900

    講義23の内容を反映

--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,30 +1,12 @@
 package raisetech.student.management;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student")
-  List<Student> searchByAll();
-
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
-
-  @Insert("INSERT student (name, age) values(#{name}, #{age})")
-  void registerStudent(String name, int age);
-
-  @Update("UPDATE student SET name = #{name} WHERE age = #{age}")
-  void updateStudentName(String name, int age);
-
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudentAge(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
+  @Select("SELECT * FROM students")
+  List<Student> search();
 }
